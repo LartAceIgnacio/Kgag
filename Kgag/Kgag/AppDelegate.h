040b364c790af5698d154ab179f4ad17a0eb9d8b
LartIgnacio <ace.ignacio@gmail.com>
2015-09-19 14:35:31 +0800

    Initial commit of the project.

---
+++ Kgag/Kgag/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Kgag
+//
+//  Created by Lart Ace Ignacio on 9/19/15.
+//  Copyright (c) 2015 Lart Ace Ignacio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Kgag/Kgag/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Kgag
+//
+//  Created by Lart Ace Ignacio on 9/19/15.
+//  Copyright (c) 2015 Lart Ace Ignacio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
