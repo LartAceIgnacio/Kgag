93b4eab12a7988aadd1f32c53a236f45fb1e93ee
LartAceIgnacio <l.ignacio@klab.com>
2015-09-28 14:30:00 +0800

    Create a KGURLPath

---
+++ Kgag/Kgag/KGURLPath.h
@@ -0,0 +1,13 @@
+//
+//  KGURLPath.h
+//  Kgag
+//
+//  Created by Lart Ace Ignacio on 9/28/15.
+//  Copyright (c) 2015 Lart Ace Ignacio. All rights reserved.
+//
+
+#define urlMainPath                      @"http://localhost/kgag/public/"
+
+//URL Path
+#define urlViewUser                       @"user/view/%@"
+#define urlAddUser                        @"user/add"
